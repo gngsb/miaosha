1183f1756fa9b376ff2bd011e50578cf53451067
liuxt <1738997412@qq.com>
2021-04-25 14:24:39 +0800

    add otp

--- src/main/java/com/miaoshaproject/controller/UserController.java
+++ src/main/java/com/miaoshaproject/controller/UserController.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 @Controller("user")
 @RequestMapping("/user")
@@ -23,6 +24,27 @@ public class UserController extends BaseController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
+    //用户获取otp短信接口
+    @RequestMapping("/getotp")
+    @ResponseBody
+    public CommonReturnType getOtp(@RequestParam(name = "telphone")String telphone){
+        //需要按照一定的规则生成OTP验证码
+        Random random = new Random();
+        int randomInt = random.nextInt(99999);
+        randomInt += 10000;
+        String otpCode = String.valueOf(randomInt);
+        //将otp验证码同对应用户的手机号关联,使用httpsession的方式绑定他的手机号与OTPCODE
+        httpServletRequest.getSession().setAttribute(telphone,otpCode);
+
+        //将otp验证码通过短信发送到用户的手机上
+        System.out.println("telphone:"+telphone+";otpCode:"+otpCode);
+
+        return CommonReturnType.create(null);
+    }
 
     @RequestMapping("/get")
     @ResponseBody
