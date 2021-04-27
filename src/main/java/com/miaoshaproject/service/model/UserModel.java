ab77a44ab9d026842202b6dd6287d9a5c65d48f0
liuxt <1738997412@qq.com>
2021-04-27 10:43:49 +0800

    add entity verification

--- src/main/java/com/miaoshaproject/service/model/UserModel.java
+++ src/main/java/com/miaoshaproject/service/model/UserModel.java
@@ -1,14 +1,27 @@
 package com.miaoshaproject.service.model;
 
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class UserModel {
     private Integer id;
+    @NotBlank(message = "用户名不能为空")
     private String name;
+    @NotNull(message = "性别为必填项")
     private Byte gender;
+    @NotNull(message = "年龄为必填项")
+    @Min(value = 0,message = "年龄必须大于0岁")
+    @Max(value = 256,message = "年龄必须小于256岁")
     private Integer age;
+    @NotBlank(message = "手机号不能为空")
     private String telphone;
     private String registerMode;
     private String thirdPartyId;
-
+    @NotBlank(message = "密码不能为空")
     private String encrptPassword;
 
     public String getEncrptPassword() {
