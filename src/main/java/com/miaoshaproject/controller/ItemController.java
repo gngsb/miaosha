f733fb7e7422a5999776fea88efffda3691e5ede
liuxt <1738997412@qq.com>
2021-04-28 14:12:02 +0800

    add product view

--- src/main/java/com/miaoshaproject/controller/ItemController.java
+++ src/main/java/com/miaoshaproject/controller/ItemController.java
@@ -8,11 +8,11 @@ import com.miaoshaproject.service.model.ItemModel;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller("/item")
 @RequestMapping("/item")
@@ -23,6 +23,8 @@ public class ItemController extends BaseController {
     private ItemService itemService;
 
     //创建商品
+    @RequestMapping(value = "/createItem",method = {RequestMethod.POST})
+    @ResponseBody
     public CommonReturnType createItem(@RequestParam(name = "title")String title,
                                        @RequestParam(name = "price")BigDecimal price,
                                        @RequestParam(name = "description")String description,
@@ -38,6 +40,27 @@ public class ItemController extends BaseController {
         ItemModel itemModelForReturn = itemService.createItem(itemModel);
         ItemVO itemVO = convertVOFromModel(itemModelForReturn);
         return CommonReturnType.create(itemVO);
+    }
+
+    //获取商品详情
+    @RequestMapping(value = "getItem",method = {RequestMethod.GET})
+    @ResponseBody
+    public CommonReturnType getItem(@RequestParam(name = "id")Integer id){
+        ItemModel itemModel = itemService.getItemById(id);
+        ItemVO itemVO = convertVOFromModel(itemModel);
+        return CommonReturnType.create(itemVO);
+    }
+
+    //获取商品列表
+    @RequestMapping(value = "/list",method = {RequestMethod.GET})
+    @ResponseBody
+    public CommonReturnType listItem(){
+        List<ItemModel> itemModel = itemService.listItem();
+        List<ItemVO> itemVOList = itemModel.stream().map(res->{
+            ItemVO itemVO = convertVOFromModel(res);
+            return itemVO;
+        }).collect(Collectors.toList());
+        return CommonReturnType.create(itemVOList);
     }
 
     private ItemVO convertVOFromModel(ItemModel itemModel){
